72a5c7974c280381fb48eff820902840025212e4
kangwook1 <rkddnr2078@naver.com>
2024-02-20 00:07:16 +0900

    commercial 도메인

---
+++ Server/src/main/java/InuCapstone/Server/domain/commercial/Commercial.java
@@ -0,0 +1,13 @@
+package InuCapstone.Server.domain.commercial;
+
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Commercial {
+
+}
