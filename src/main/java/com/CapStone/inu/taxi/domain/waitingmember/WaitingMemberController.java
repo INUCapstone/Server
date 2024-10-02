5b146c882678eb0bde7befcbf89c425cc62cb060
kangwook1 <rkddnr2078@naver.com>
2024-10-02 14:40:13 +0900

    웹소켓 연결 취소시 매칭 취소 로직 추가

--- src/main/java/com/CapStone/inu/taxi/domain/waitingmember/WaitingMemberController.java
+++ src/main/java/com/CapStone/inu/taxi/domain/waitingmember/WaitingMemberController.java
@@ -2,11 +2,14 @@ package com.CapStone.inu.taxi.domain.waitingmember;
 
 import com.CapStone.inu.taxi.domain.waitingmember.dto.WaitingMemberReqDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +18,22 @@ public class WaitingMemberController {
 
     @MessageMapping("match/{memberId}")
     public void startMatching(@DestinationVariable Long memberId, @Payload WaitingMemberReqDto waitingMemberReqDto){
-        waitingMemberService.startMatching(memberId,waitingMemberReqDto);
+        waitingMemberService.createWaitingMember(memberId,waitingMemberReqDto);
+
     }
 
-    @GetMapping(value = "/test")
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        // StompHeaderAccessor를 통해 메시지에서 세션 정보를 가져옵니다.
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        // 세션 속성에서 memberId를 가져옵니다.
+        Long memberId = (Long) headerAccessor.getSessionAttributes().get("memberId");
+        waitingMemberService.cancelMatching(memberId);
+
+    }
+
+        @GetMapping(value = "/test")
     public void test() {
         waitingMemberService.matchUser(1L);
     }
