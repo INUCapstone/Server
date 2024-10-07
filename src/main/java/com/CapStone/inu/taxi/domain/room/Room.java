93b4ac92604d338c0a56ab071afb01718c02b561
kangwook1 <rkddnr2078@naver.com>
2024-10-07 15:36:08 +0900

    room에서 waitingMember 없애기

--- src/main/java/com/CapStone/inu/taxi/domain/room/Room.java
+++ src/main/java/com/CapStone/inu/taxi/domain/room/Room.java
@@ -1,6 +1,5 @@
 package com.CapStone.inu.taxi.domain.room;
 
-import com.CapStone.inu.taxi.domain.waitingmember.WaitingMember;
 import com.CapStone.inu.taxi.domain.waitingmemberRoom.WaitingMemberRoom;
 import com.CapStone.inu.taxi.global.common.BaseEntity;
 import jakarta.persistence.*;
@@ -9,6 +8,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -41,15 +41,12 @@ public class Room extends BaseEntity {
     @Column(name = "is_delete", nullable = false)
     private Boolean isDelete;
 
-    @OneToMany(mappedBy = "room", cascade = CascadeType.PERSIST, orphanRemoval = true)
-    private List<WaitingMember> waitingMemberList;
-
     @OneToMany(mappedBy = "room", fetch = FetchType.LAZY)
-    private List<WaitingMemberRoom> waitingMemberRoomList;
+    private List<WaitingMemberRoom> waitingMemberRoomList=new ArrayList<>();
 
     @Builder
     private Room(Integer taxiFare, Integer taxiDuration, String taxiPath, Integer taxiHeadcount,
-                 Long driverId, List<WaitingMember> waitingMemberList) {
+                 Long driverId) {
         this.taxiFare = taxiFare;
         this.taxiDuration = taxiDuration;
         this.taxiPath = taxiPath;
@@ -57,6 +54,5 @@ public class Room extends BaseEntity {
         this.driverId = driverId;
         isStart = false;
         isDelete = false;
-        this.waitingMemberList = waitingMemberList;
     }
 }
